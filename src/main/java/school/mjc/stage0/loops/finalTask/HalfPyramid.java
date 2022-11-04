c326a50898477310c0fc3a06a64d93ce86d995b0
Maryna Kidun <marykidun@gmail.com>
2022-11-04 10:15:03 +0200

    Implemented all tasks

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,10 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = cathetusLength; i > 0; i--) {
+            for (int s = 1; s < i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = cathetusLength; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
     }
 }
